ca5aa032471f8c2e49973593d8bd66f107d2aaed
Sean Carolan <scarolan@gmail.com>
2015-05-29 13:38:16 -0500

    Added ChefDK to installer.

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,3 +2,4 @@
 default['ewcw']['admin']['username'] = 'Chef'
 default['ewcw']['admin']['password'] = 'password'
 default['ewcw']['choco_packages'] = { git: '1.9.5.20150320', sublime: '2.0.2', conemu: '15.5.13.0' }
+default['ewcw']['chefdk']['url'] = 'https://opscode-omnibus-packages.s3.amazonaws.com/windows/2008r2/x86_64/chefdk-0.6.0-1.msi'
--- recipes/packages.rb
+++ recipes/packages.rb
@@ -4,23 +4,24 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
-# Install chocolatey
-#powershell_script "install_chocolatey" do
-#  action :run
-#  code "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))"
-#end
-
+# Leverage the community Chocolatey cookbook
 include_recipe "chocolatey"
 
-# Install packages from chocolatey
+# Install chocolatey packages
 node['ewcw']['choco_packages'].each do |package_name, package_version|
-  #log "package name" do
-  #  message "#{package_name}, #{package_version}"
-  #  level :warn
-  #end
-
   chocolatey package_name do
     action :install
     version package_version
   end
+end
+
+# Install the ChefDK
+remote_file "#{Chef::Config['file_cache_path']}/ChefDK.msi" do
+  action :create
+  source node['ewcw']['chefdk']['url']
+end
+
+windows_package "ChefDK" do
+  action :install
+  source "#{Chef::Config['file_cache_path']}/ChefDK.msi"
 end
